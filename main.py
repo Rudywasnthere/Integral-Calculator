78069ee0d6429501b16ece6cab6c9922a553650d
Rudywasnthere <rudybcchs@gmail.com>
2020-10-21 17:52:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import time, sys
+
+print("Hello, this is a definite integral calculator")
+time.sleep(1)
+function= input("First, what is your function:\n")
+print("Second, on what interval are you integrating?")
+lower_limit= input("Left Limit:")
+upper_limit= input("Right Limit")
